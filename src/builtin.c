4d730187557af052d21e9ef079f412e715abd2b0
Anastasia Shalaginova Zetzner <anastasia@MacBook-Pro-de-Anastasia.local>
2023-10-05 07:39:35 +0200

    05_10

--- src/builtin.c
+++ src/builtin.c
@@ -6,7 +6,7 @@
 /*   By: ashalagi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 11:21:48 by ashalagi          #+#    #+#             */
-/*   Updated: 2023/10/04 13:37:00 by ashalagi         ###   ########.fr       */
+/*   Updated: 2023/10/05 07:38:20 by ashalagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,20 @@ int		execute_builtin(t_data *data)
 
 	result = 0;
 	if (ft_strcmp(data->command, "echo") == 0)
-		result = ft_echo(data->arguments); // Adjusted based on assumed ft_echo signature
+		result = ft_echo(data->arguments);
 	if (ft_strcmp(data->command, "cd") == 0)
-        result = ft_cd(data); // Adjusted based on assumed ft_cd signature
+        result = ft_cd(data);
 	if (ft_strcmp(data->command, "pwd") == 0)
-		result = ft_pwd(); // No adjustment needed if ft_pwd doesn't require parameters
+		result = ft_pwd();
 	if (ft_strcmp(data->command, "env") == 0)
-		ft_env(data->envp); // Adjusted based on assumed ft_env signature
+		ft_env(data->envp);
 	if (ft_strcmp(data->command, "export") == 0)
-		result = ft_export(data->arguments); // Adjusted based on assumed ft_export signature
+		result = ft_export(data->arguments);
 	if (ft_strcmp(data->command, "unset") == 0)
-		result = ft_unset(data->arguments, data->envp); // Adjusted based on assumed ft_unset signature
+		result = ft_unset(data->arguments, data->envp);
 	return (result);
 }
+
 
 
 /*
